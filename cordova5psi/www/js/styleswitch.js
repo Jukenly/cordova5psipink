3c28ab94e25672492d8b3e446dee624b27400285
Jukenly <adiang.huang@gmail.com>
2020-12-21 23:04:37 +0700

    cordova 5psi pink

---
+++ cordova5psi/www/js/styleswitch.js
@@ -0,0 +1,19 @@
+/* Style Switcher */
+$(document).ready(function () {
+
+    var styleswitcherstr = ' \
+	<h2>Style Switcher <a href="#"></a></h2> \
+    <div class="content"> \
+    <div class="clear"></div> \
+    <h3>Light Theme</h3> \
+	<a href="../Facebuk-Dark/index.html" class="white-style">Dark Theme</a> \
+    </div><!-- End content --> \
+	';
+
+
+    var wrap = $('<div/>');
+    wrap.addClass('switcher');
+    wrap.append(styleswitcherstr);
+    $("#copyright").after(wrap);
+
+});
